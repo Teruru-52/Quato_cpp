ea18a875991e744f430ca0df3bb4d0d0e16e9274
Teruru-52 <83484632+Teruru-52@users.noreply.github.com>
2022-05-09 22:04:10 +0900

    add ir sensor class

---
+++ Core/Inc/hardware/ir_sensor.hpp
@@ -0,0 +1,50 @@
+#ifndef HARDWARE_IR_SENSOR_HPP_
+#define HARDWARE_IR_SENSOR_HPP_
+
+#include "main.h"
+#include "hardware/led.hpp"
+
+namespace hardware
+{
+    class IRsensor
+    {
+    public:
+        IRsensor(uint32_t threshold);
+        // IRsensor(int sampling_count);
+
+        void on_front_led();
+        void on_side_led();
+        void off_front_led();
+        void off_side_led();
+        void on_all_led();
+        void off_all_led();
+
+        void UI_led_onoff();
+
+        void UpdateSideValue();
+        void UpdateFrontValue();
+        void Update();
+        float GetBatteryVoltage();
+
+    private:
+        LED led;
+        const int sampling_count = 16;
+        uint32_t threshold; // 2100
+
+        uint16_t dma_f[3];
+        uint16_t dma_b[2];
+
+        uint32_t fl[16];
+        uint32_t fr[16];
+        uint32_t sl[16];
+        uint32_t sr[16];
+
+        uint32_t ir_fl;
+        uint32_t ir_fr;
+        uint32_t ir_sl;
+        uint32_t ir_sr;
+
+        float bat_vol;
+    };
+}
+#endif //  HARDWARE_IR_SENSOR_HPP_
---
+++ Core/Src/hardware/ir_sensor.cpp
@@ -0,0 +1,128 @@
+#include "hardware/ir_sensor.hpp"
+
+namespace hardware
+{
+    IRsensor::IRsensor(uint32_t threshold)
+        : threshold(threshold) {}
+
+    void IRsensor::on_front_led()
+    {
+        __HAL_TIM_SET_COMPARE(&htim10, TIM_CHANNEL_1, 200);
+    }
+
+    void IRsensor::on_side_led()
+    {
+        __HAL_TIM_SET_COMPARE(&htim11, TIM_CHANNEL_1, 200);
+    }
+
+    void IRsensor::off_front_led()
+    {
+        __HAL_TIM_SET_COMPARE(&htim10, TIM_CHANNEL_1, 0);
+    }
+
+    void IRsensor::off_side_led()
+    {
+        __HAL_TIM_SET_COMPARE(&htim11, TIM_CHANNEL_1, 0);
+    }
+
+    void IRsensor::on_all_led()
+    {
+        on_front_led();
+        on_side_led();
+    }
+
+    void IRsensor::off_all_led()
+    {
+        off_front_led();
+        off_side_led();
+    }
+
+    void IRsensor::UI_led_onoff()
+    {
+        if (ir_fl > threshold)
+            led.on_front_left();
+        else
+            led.off_front_left();
+
+        if (ir_fr > threshold)
+            led.on_front_right();
+        else
+            led.off_front_right();
+
+        if (ir_sl > threshold)
+            led.on_side_left();
+        else
+            led.off_side_left();
+
+        if (ir_sr > threshold)
+            led.on_side_right();
+        else
+            led.off_side_right();
+    }
+
+    void IRsensor::UpdateSideValue()
+    {
+        HAL_ADC_Start_DMA(&hadc2, (uint32_t *)dma_b, 2);
+
+        for (int i = sampling_count - 1; i > 0; i--)
+        {
+            sl[i] = sl[i - 1];
+            sr[i] = sr[i - 1];
+        }
+        sl[0] = dma_b[0];
+        sr[0] = dma_b[1];
+
+        ir_sl = dma_b[0];
+        ir_sr = dma_b[1];
+    }
+
+    void IRsensor::UpdateFrontValue()
+    {
+        HAL_ADC_Start_DMA(&hadc1, (uint32_t *)dma_f, 3);
+
+        for (int i = sampling_count - 1; i > 0; i--)
+        {
+            fl[i] = fl[i - 1];
+            fr[i] = fr[i - 1];
+        }
+        fl[0] = dma_f[0];
+        fr[0] = dma_f[1];
+
+        ir_fl = dma_f[0];
+        ir_fr = dma_f[1];
+
+        bat_vol = (float)dma_f[2] * 3.3 / 4096 * 3;
+    }
+
+    void IRsensor::Update()
+    {
+        uint32_t max_fl = 0;
+        uint32_t max_fr = 0;
+        uint32_t max_sl = 0;
+        uint32_t max_sr = 0;
+
+        for (int i = sampling_count - 1; i >= 0; i--)
+        {
+            if (fl[i] > max_fl)
+                max_fl = fl[i];
+            if (fr[i] > max_fr)
+                max_fr = fr[i];
+            if (sl[i] > max_sl)
+                max_sl = sl[i];
+            if (sr[i] > max_sr)
+                max_sr = sr[i];
+        }
+
+        ir_fl = max_fl;
+        ir_fr = max_fr;
+        ir_sl = max_sl;
+        ir_sr = max_sr;
+
+        UI_led_onoff();
+    }
+
+    float IRsensor::GetBatteryVoltage()
+    {
+        return bat_vol;
+    }
+}
