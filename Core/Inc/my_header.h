b8bea1fd2c653fefc3f96cca8a300d18af3dcd71
Teruru-52 <83484632+Teruru-52@users.noreply.github.com>
2022-05-09 22:05:06 +0900

    add includes

--- Core/Inc/my_header.h
+++ Core/Inc/my_header.h
@@ -5,6 +5,8 @@
 #include "hardware/motor.hpp"
 #include "hardware/encoder.hpp"
 #include "hardware/imu.hpp"
+#include "hardware/ir_sensor.hpp"
+#include "hardware/speaker.hpp"
 #include "controller.hpp"
 #include "odometory.hpp"
 #include "pid_controller.hpp"
