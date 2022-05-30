841a52c5a5e7a4e3a6f70eba203a3674e84d9296
Teruru-52 <83484632+Teruru-52@users.noreply.github.com>
2022-05-30 21:51:28 +0900

    delete useless includes

--- Core/Inc/my_header.h
+++ Core/Inc/my_header.h
@@ -2,15 +2,10 @@
 #define _MY_HEADER_H_
 
 #include "hardware/led.h"
-#include "hardware/motor.h"
-#include "hardware/encoder.h"
-#include "hardware/imu.h"
 #include "hardware/ir_sensor.h"
 #include "hardware/speaker.h"
-#include "controller.h"
+#include "controller/controller.h"
 #include "odometory.h"
-#include "pid_controller.h"
-#include "trajectory.h"
 #include "digital_filter.h"
 
 #endif // _MY_HEADER_H_
